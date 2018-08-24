631fce33dbf1b6e4041226b805883678268bda84
UnamataSanatarai <unamatasanatarai@gmail.com>
2018-08-24 13:15:15 +0200

    Grouping apt installs to be run in one go

--- src/ubuntu.sh
+++ src/ubuntu.sh
@@ -1,6 +1,22 @@
-VERSION="0.1"
+VERSION="0.1" ask_for_sudo
 
-ask_for_sudo
+APTINSTALLS="apt install -y"
+APTINSTALLS="${APTINSTALLS} apt-transport-https ca-certificates build-essential"
+APTINSTALLS="${APTINSTALLS} software-properties-common"
+APTINSTALLS="${APTINSTALLS} curl vim git htop ncdu ack shutter"
+APTINSTALLS="${APTINSTALLS} google-chrome-stable docker-ce"
+
+SNAPINSTALLS="snap install slack --classic"
+
+proclaim "Inserting important apt-repos"
+curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+if grep -q "dl.google.com/linux/chrome/deb" /etc/apt/sources.list ; then
+  echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
+fi
+if grep -q "download.docker.com/linux/ubuntu" /etc/apt/sources.list ; then
+  echo "deb https://download.docker.com/linux/ubuntu bionic stable" >> /etc/apt/sources.list
+fi
 
 proclaim "Injecting .vimrc .bash_aliases"
 cd ~
@@ -11,48 +27,14 @@ wget https://raw.githubusercontent.com/unamatasanatarai/dotfiles/master/.bash_al
 print_success "Injected .vimrc .bash_aliases"
 
 apt update
+proclaim "$APTINSTALLS"
+$APTINSTALLS
+print_success "$APTINSTALLS"
+
+proclaim "$SNAPINSTALLS"
+$SNAPINSTALLS
+print_success "$SNAPINSTALLS"
 apt -y full-upgrade
-
-proclaim "Installing bare necessities"
-apt install -y apt-transport-https ca-certificates \
-  build-essential \
-  curl software-properties-common vim git htop ncdu ack shutter
-print_success "Installed bare minimum"
-
-_=$(command -v slack)
-if [ "$?" != 0 ]; then
-  proclaim "Installing Slack"
-  snap install slack --classic
-  print_success "Slack installed"
-fi
-
-_=$(command -v google-chrome)
-if [ "$?" != 0 ]; then
-  proclaim "Adding Chrome apt-repo"
-  curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-  add-apt-repository "deb https://dl.google.com/linux/chrome/deb/ stable main"
-  print_success "Added Chrome apt-repo"
-
-  apt update
-
-  proclaim "Installing Chrome"
-  apt install -y google-chrome-stable
-  proclaim "Installed Chrome"
-fi
-
-_=$(command -v docker)
-if [ "$?" != 0 ]; then
-  proclaim "Adding Docker apt-repo"
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-  print_success "Added Docker apt-repo"
-
-  apt update
-
-  proclaim "Installing Docker"
-  apt install -y docker-ce
-  print_success "Installed docker"
-fi
 
 _=$(command -v docker-compose)
 if [ "$?" != 0 ]; then
