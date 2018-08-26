dc037ca50425d1c77860a8cbafcddd13c286f233
Piotr Grabowski <piograski@gmail.com>
2018-08-26 20:17:27 +0200

    controlling volume with keyboard

--- i3wm.sh
+++ i3wm.sh
@@ -54,7 +54,7 @@ apt install -y i3 i3blocks arc-theme \
   libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \
   libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
   libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf \
-  libxcb-xrm0 libxcb-xrm-dev automake scrot
+  libxcb-xrm0 libxcb-xrm-dev automake scrot volumeicon
 print_success "Installed important things. Allrgiht!"
 
 proclaim "Create basic directories for i3"
@@ -74,7 +74,6 @@ proclaim "Copy i3 custom scripts"
 cd ~/.config/i3/scripts
 wget https://github.com/unamatasanatarai/dotfiles/raw/master/.config/i3/scripts/lock.sh
 wget https://github.com/unamatasanatarai/dotfiles/raw/master/.config/i3/scripts/lock.png
-wget https://github.com/unamatasanatarai/dotfiles/raw/master/.config/i3/scripts/confirm_logout.sh
 wget https://github.com/unamatasanatarai/dotfiles/raw/master/.config/i3/scripts/prompt.sh
 chmod +x *.sh
 print_success "Copied i3 custom scripts"
--- src/i3wm.sh
+++ src/i3wm.sh
@@ -9,7 +9,7 @@ apt install -y i3 i3blocks arc-theme \
   libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev \
   libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
   libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf \
-  libxcb-xrm0 libxcb-xrm-dev automake scrot
+  libxcb-xrm0 libxcb-xrm-dev automake scrot volumeicon
 print_success "Installed important things. Allrgiht!"
 
 proclaim "Create basic directories for i3"
