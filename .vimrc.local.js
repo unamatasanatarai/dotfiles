6a521e4de646989445c690b35a6ff635ef8a2d6b
UnamataSanatarai <unamatasanatarai@gmail.com>
2019-03-10 14:10:01 +0100

    fix vimrc.local.js

--- .vimrc.local.js
+++ .vimrc.local.js
@@ -1,4 +1,4 @@
 let g:auto_save = 1  " enable AutoSave on Vim startup
 let b:ale_fixers = {'javascript': ['eslint']}
+let b:ale_linters = {'javascript': ['eslint']}
 let g:ale_fix_on_save = 1
-let g:ale_linters = {'javascript': ['eslint']}
