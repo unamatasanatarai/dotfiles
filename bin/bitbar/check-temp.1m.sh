11dcabe97061c6be1fbec2b6eca53f08c0ac03eb
Piotr Grabowski <p.grabowski@dhosting.pl>
2020-10-09 09:37:43 +0200

    Add light dark support to bitbar check temp

--- bin/bitbar/check-temp.1m.sh
+++ bin/bitbar/check-temp.1m.sh
@@ -2,13 +2,25 @@
 # install bitbar.app
 # install osx-cpu-temp
 
-temp=`/usr/local/bin/osx-cpu-temp`
+theme=$(defaults read "Apple Global Domain" AppleInterfaceStyle 2>&1 > /dev/null)
+if [[ $? -eq 1 ]]; then
+  # LIGHT
+  col_normal="#111111"
+  col_warning="#F78502"
+  col_danger="#f7f0202"
+elif [[ $theme -eq "Dark" ]]; then
+  col_normal="#EEEEEE"
+  col_warning="#F78502"
+  col_danger="#f7f0202"
+fi
+
+temp=`osx-cpu-temp`
 num=${temp:0:2}
-color="#000000"
+color=$col_normal
 if [[ $num > 60 ]]; then
-  color="#fcf82a"
+  color=$col_warning
 fi
 if [[ $num > 75 ]]; then
-  color="#ff0000"
+  color=$col_danger
 fi
 echo "$temp | color=$color"
