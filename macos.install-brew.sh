827e143bbbfbb4337fb6695fc3afc8dd53c7a908
Piotr Grabowski <p.grabowski@dhosting.pl>
2020-10-09 10:53:55 +0200

    add cask kap slack vlc; prettier bash git showing

--- macos.install-brew.sh
+++ macos.install-brew.sh
@@ -5,7 +5,7 @@ echo "brew update"
 brew update
 
 echo "install my apps"
-brew cask install iterm2 brave-browser lulu sublime-text bitbar eloston-chromium tor-browser keepassxc
+brew cask install iterm2 brave-browser lulu sublime-text bitbar eloston-chromium tor-browser keepassxc kap slack vlc
 
 echo "install my tools"
 brew install vim htop curl wget git tmux ack bash node yarn mariadb apache2 fzf freetype
