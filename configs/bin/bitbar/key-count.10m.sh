e143be997cd36685e09d5dd39ee64d79c2b2e564
UnamataSanatarai <unamatasanatarai@gmail.com>
2020-11-30 08:45:16 +0100

    Fix error in key counter

--- configs/bin/bitbar/key-count.10m.sh
+++ configs/bin/bitbar/key-count.10m.sh
@@ -2,8 +2,10 @@
 # install bitbar.app
 # install osx-cpu-temp
 
-count=$(wc -l ~/.keylogger/$(date "+%Y%m%d").log)
-num=$(echo $count | grep -o -E '^\s*[0-9]+')
-words=$( echo $num / 5 | bc)
-echo "${num}c ${words}w"
-
+filename=~/.keylogger/$(date "+%Y%m%d").log
+if [ -f $filename ]; then
+    count=$(wc -l $filename)
+    num=$(echo $count | grep -o -E '^\s*[0-9]+')
+    words=$( echo $num / 5 | bc)
+    echo "${num}c ${words}w"
+fi
