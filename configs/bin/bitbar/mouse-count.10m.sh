a1b3d8d40493bb7fd47a1600d4b379fa71bfdead
UnamataSanatarai <unamatasanatarai@gmail.com>
2020-12-03 14:55:49 +0100

    bitbar stats for keys and mouse in new location

--- configs/bin/bitbar/mouse-count.10m.sh
+++ configs/bin/bitbar/mouse-count.10m.sh
@@ -2,10 +2,9 @@
 # install bitbar.app
 # install osx-cpu-temp
 
-filename=~/.cache/mousekeylogs/k$(date "+%Y%m%d").log
+filename=~/.cache/mousekeylogs/m$(date "+%Y%m%d").log
 if [ -f $filename ]; then
     count=$(wc -l $filename)
     num=$(echo $count | grep -o -E '^\s*[0-9]+')
-    words=$( echo $num / 5 | bc)
-    echo "${num}c ${words}w"
+    echo "${num}M"
 fi
