6058cfd121714965d9d847a8b5d9fae54294ff2a
UnamataSanatarai <unamatasanatarai@gmail.com>
2020-12-07 08:58:52 +0100

    fix file exists problem with mosue-counter

--- configs/bin/bitbar/mouse-count.10m.sh
+++ configs/bin/bitbar/mouse-count.10m.sh
@@ -3,12 +3,12 @@
 # install osx-cpu-temp
 
 filename=~/.cache/mousekeylogs/m$(date "+%Y%m%d").log
-left=$(grep "L" $filename | wc -l | grep -o -E '[0-9]+')
-right=$(grep "R" $filename | wc -l | grep -o -E '[0-9]+')
-wheel=$(grep "W" $filename | wc -l | grep -o -E '[0-9]+')
-other=$(grep "O" $filename | wc -l | grep -o -E '[0-9]+')
-total=$(echo $left + $right + $other | bc)
 if [ -f $filename ]; then
+    left=$(grep "L" $filename | wc -l | grep -o -E '[0-9]+')
+    right=$(grep "R" $filename | wc -l | grep -o -E '[0-9]+')
+    wheel=$(grep "W" $filename | wc -l | grep -o -E '[0-9]+')
+    other=$(grep "O" $filename | wc -l | grep -o -E '[0-9]+')
+    total=$(echo $left + $right + $other | bc)
     echo "${total}m"
     echo "---"
     echo -e "left\t\t${left}"
