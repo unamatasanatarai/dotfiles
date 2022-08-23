64bb00f0a8a17aa043ce23c4a5f44b5917397896
jarmos <jarmos@example.com>
2022-08-23 10:38:49 +0200

    WIP

--- stow/.config/nvim/lua/defaults/lsp/null-ls.lua
+++ stow/.config/nvim/lua/defaults/lsp/null-ls.lua
@@ -5,6 +5,7 @@ local formatting = null_ls.builtins.formatting
 local sources = {
   formatting.eslint,
   formatting.black,
+  formatting.shfmt.with({extra_args={"-i", "2"} }),
 }
 
 null_ls.setup({
