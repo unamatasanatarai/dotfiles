8b7f9ecbcc1e37932b47958120dd7928eede2457
Unamata Sanatarai <unamatasanatarai@gmail.com>
2022-08-25 21:01:43 +0200

    WIP

--- stow/.config/nvim/lua/defaults/lsp/null-ls.lua
+++ stow/.config/nvim/lua/defaults/lsp/null-ls.lua
@@ -3,10 +3,11 @@ local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 
 local sources = {
---  formatting.eslint,
+  --  formatting.eslint,
   formatting.black,
-  formatting.prettier,
-  formatting.shfmt.with({extra_args={"-i", "2"} }),
+  formatting.prettier.with({ filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css",
+    "scss", "less", "json", "jsonc", "yaml", "markdown", "graphql", "handlebars" } }),
+  formatting.shfmt.with({ extra_args = { "-i", "2" } }),
 }
 
 null_ls.setup({
