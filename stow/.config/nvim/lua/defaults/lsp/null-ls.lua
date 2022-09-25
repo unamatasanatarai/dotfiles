4ebd8aa9d3b9367da6f3561cfb034fd3f4461de8
Unamata Sanatarai <unamatasanatarai@gmail.com>
2022-09-25 08:30:27 +0200

    reset LSP

--- stow/.config/nvim/lua/defaults/lsp/null-ls.lua
+++ stow/.config/nvim/lua/defaults/lsp/null-ls.lua
@@ -7,37 +7,36 @@ local code_actions = null_ls.builtins.code_actions
 local sources = {
   diagnostics.shellcheck,
   code_actions.shellcheck,
-  --  formatting.eslint,
-  formatting.black,
-  formatting.shellharden.with({
-    filetypes = { 'sh' }
-  }),
-  formatting.prettier.with({
-    filetypes = {
-      "javascript",
-      "javascriptreact",
-      "typescript",
-      "typescriptreact",
-      "vue",
-      "css",
-      "scss",
-      "less",
-      "json",
-      "jsonc",
-      "yaml",
-      "markdown",
-      "graphql",
-      "handlebars",
-    }
-  }),
-  formatting.shfmt.with({
-    extra_args = {
-      "-i",
-      "2",
-      "-ci",
-      "-p"
-    }
-  }),
+--  --  formatting.eslint,
+--  formatting.shellharden.with({
+--    filetypes = { 'sh' }
+--  }),
+--  formatting.prettier.with({
+--    filetypes = {
+--      "javascript",
+--      "javascriptreact",
+--      "typescript",
+--      "typescriptreact",
+--      "vue",
+--      "css",
+--      "scss",
+--      "less",
+--      "json",
+--      "jsonc",
+--      "yaml",
+--      "markdown",
+--      "graphql",
+--      "handlebars",
+--    }
+--  }),
+--  formatting.shfmt.with({
+--    extra_args = {
+--      "-i",
+--      "2",
+--      "-ci",
+--      "-p"
+--    }
+--  }),
 }
 
 null_ls.setup({
