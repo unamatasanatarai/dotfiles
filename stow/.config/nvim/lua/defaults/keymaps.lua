da20cd4fb0d69653843e95ba397cbd742e3186a8
Unamata Sanatarai <unamatasanatarai@gmail.com>
2022-09-25 12:34:17 +0200

    cleanup keymaps for nvim

--- stow/.config/nvim/lua/defaults/keymaps.lua
+++ stow/.config/nvim/lua/defaults/keymaps.lua
@@ -1,18 +1,13 @@
 local opts = { noremap = true, silent = true }
-
---local term_opts = { silent = true }
-
--- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
---Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 keymap("i", "jj", "<ESC>", opts)
+keymap("i", "jk", "<ESC>", opts)
 keymap("n", "<leader>w", ":w<cr>", opts)
-keymap("n", "<leader>bp", ":lua vim.lsp.buf.formatting_sync()<CR>", opts)
 
 keymap("n", "<C-p>", ":Telescope find_files<cr>", opts)
 keymap("n", "<C-g>", ":Telescope live_grep<cr>", opts)
