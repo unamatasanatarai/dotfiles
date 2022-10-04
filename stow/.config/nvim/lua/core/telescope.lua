ee4e6e085ca88d0ad8d2fd90e4883870abfd4149
jarmos <jarmos@example.com>
2022-10-04 11:24:47 +0200

    Telescope ignore files&folders

--- stow/.config/nvim/lua/core/telescope.lua
+++ stow/.config/nvim/lua/core/telescope.lua
@@ -9,7 +9,7 @@ telescope.setup {
 
   pickers = {
     live_grep = {
-      additional_args = function(opts)
+      additional_args = function(_)
         return { "--hidden", "--no-ignore" }
       end
     },
@@ -22,6 +22,55 @@ telescope.setup {
     prompt_prefix = "❯ ",
     selection_caret = "❯ ",
     path_display = { "smart" },
+    file_ignore_patterns = {
+      ".git/",
+      "docs/",
+      "vendor/*",
+      "%.lock",
+      "__pycache__/*",
+      "%.sqlite3",
+      "%.ipynb",
+      "node_modules/*",
+      "%.svg",
+      "%.otf",
+      "%.ttf",
+      "%.webp",
+      ".dart_tool/",
+      ".github/",
+      ".gradle/",
+      ".idea/",
+      ".settings/",
+      ".vscode/",
+      "__pycache__/",
+      "build/",
+      "env/",
+      "gradle/",
+      "node_modules/",
+      "%.pdb",
+      "%.dll",
+      "%.class",
+      "%.exe",
+      "%.cache",
+      "%.ico",
+      "%.pdf",
+      "%.dylib",
+      "%.jar",
+      "%.docx",
+      "%.met",
+      "smalljre_*/*",
+      ".vale/",
+      "%.burp",
+      "%.mp4",
+      "%.mkv",
+      "%.rar",
+      "%.zip",
+      "%.7z",
+      "%.tar",
+      "%.bz2",
+      "%.epub",
+      "%.flac",
+      "%.tar.gz",
+    },
 
     mappings = {
       i = {
