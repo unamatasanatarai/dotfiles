b5fae4f218ede2e61d79f693df6be790282c9bc5
jarmos <jarmos@example.com>
2022-10-12 15:23:19 +0200

    hammerspoon config and automated reload + hotkey to reload config

--- stow/.config/.hammerspoon/init.lua
+++ stow/.config/.hammerspoon/init.lua
@@ -7,7 +7,29 @@
 -- system_profiler SPUSBDataType
 -- system_profiler SPBluetoothDataType
 local FR = require('foundation_remapping')
-local logiremap = FR.new ( { vendorID = 0x046D, productID = 0xB359 })
+local logiremap = FR.new({ vendorID = 0x046D, productID = 0xB359 })
 logiremap:remap("rcmd", "ralt")
 logiremap:register()
 
+
+hs.pathwatcher.new(os.getenv("HOME") .. "/.config/.hammerspoon/", hs.reload):start()
+hs.alert.show(
+  "HS CONFIG RELOADED",
+  {
+    strokeWidth     = 2,
+    strokeColor     = { white = 1, alpha = 0.75 },
+    fillColor       = { white = 0, alpha = 0.75 },
+    textColor       = { white = 1, alpha = 0.75 },
+    textFont        = ".AppleSystemUIFont",
+    textSize        = 10,
+    radius          = 1,
+    atScreenEdge    = 1,
+    fadeInDuration  = 0.1,
+    fadeOutDuration = 0.1,
+    padding         = 1,
+  },
+  hs.screen.mainScreen(),
+  .3
+)
+
+hs.hotkey.bind({ "alt", "shift" }, "[", hs.reload)
