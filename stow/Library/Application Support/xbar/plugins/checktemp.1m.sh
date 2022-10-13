c2466344e7f815505ae411f194fef1848720fb56
jarmos <jarmos@example.com>
2022-10-13 09:36:22 +0200

    checktemp fails too often. hardcoded paths

--- stow/Library/Application Support/xbar/plugins/checktemp.1m.sh
+++ stow/Library/Application Support/xbar/plugins/checktemp.1m.sh
@@ -3,7 +3,11 @@
 set -euo pipefail
 
 if [[ "$(uname -m)" = "x86_64" ]]; then
-  "$(brew --prefix)"/bin/osx-cpu-temp
+  if [ -f /usr/local/bin/osx-cpu-temp ]; then
+    /usr/local/bin/osx-cpu-temp
+  else
+    /opt/homebrew/bin/osx-cpu-temp
+  fi
 else
   echo "?°C"
 fi
