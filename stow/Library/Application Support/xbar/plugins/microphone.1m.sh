e20c742d2a34339d095353df8a2429dacec6a77b
jarmos <jarmos@example.com>
2022-10-13 10:58:22 +0200

    numbers next to microphone in xbar

--- stow/Library/Application Support/xbar/plugins/microphone.1m.sh
+++ stow/Library/Application Support/xbar/plugins/microphone.1m.sh
@@ -4,8 +4,4 @@ set -euo pipefail
 
 input_volume=$(osascript -e "get input volume of (get volume settings)")
 
-if [ "$input_volume" -lt 20 ]; then
-  echo "✕"
-else
-  echo "🎤"
-fi
+echo "$input_volume🎤"
