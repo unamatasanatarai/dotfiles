6d7d6b78ea50261be9ab2b823c153bbeda10a623
Unamata Sanatarai <unamatasanatarai@gmail.com>
2022-10-15 23:24:38 +0200

    nl

--- stow/.config/nvim/lua/core/plugins.lua
+++ stow/.config/nvim/lua/core/plugins.lua
@@ -85,6 +85,7 @@ return packer.startup(function(use)
       require('crates').setup()
     end,
   }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
