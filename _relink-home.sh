ab3cc620db7961bb75c136dfba1c5d9fefeed71c
Unamata Sanatarai <unamatasanatarai@gmail.com>
2025-11-25 12:42:36 +0100

    fix keyboard bindings for bash

--- _relink-home.sh
+++ _relink-home.sh
@@ -11,6 +11,7 @@ symlink_config() {
 symlink_config "$HOME/.config/bash/bashrc" "$HOME/.bashrc" 
 symlink_config "$HOME/.config/bash/bashrc" "$HOME/.bash_profile" 
 symlink_config "$HOME/.config/bash/bashrc" "$HOME/.profile" 
+symlink_config "$HOME/.config/bash/inputrc" "$HOME/.inputrc" 
 symlink_config "$HOME/.config/bash/bash_logout" "$HOME/.bash_logout" 
 symlink_config "$PWD/configs/bin" "$HOME/bin" 
 symlink_config "$HOME/.config/vim/vimrc" "$HOME/.vimrc" 
