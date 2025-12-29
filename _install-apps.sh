c78f8b759dcf119c928c8340f9e62f09588df8a6
Unamata Sanatarai <unamatasanatarai@gmail.com>
2025-12-29 23:52:36 +0100

    WIP

--- _install-apps.sh
+++ _install-apps.sh
@@ -5,4 +5,8 @@ if ! command -v brew &>/dev/null; then
 fi
 
 brew bundle install --file=./configs/Brewfile
+
 cp ./configs/fonts/source-code-pro/* /Library/Fonts/
+cp ./configs/fonts/archivo/*.ttf /Library/Fonts/
+cp ./configs/fonts/archivo/static/*.ttf /Library/Fonts/
+fc-cache -fv > /dev/null
